132b3d829de1386449777f2bd1a59a98b0286c70
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2025-12-19 00:29:23 -0500

    Hark, Sparks! (#491)
    * Basic improvements to sparks
    
    * Pull spark configuration out of SparkOnHit component
    
    * Add various sparks to tasers, batons, RCD
    
    * Add sparks back to electrocution
    
    * Adjust new projectiles
    
    * Tile fires on cable cuts
    
    * That's a terrible idea, actually
    
    * Add a custom PreventCollide system
    
    * Make the new prevent collide component prevent collisions

--- Content.Shared/_ES/Sparks/Components/ESSparkOnHitComponent.cs
+++ Content.Shared/_ES/Sparks/Components/ESSparkOnHitComponent.cs
@@ -1,6 +1,5 @@
 using Content.Shared.FixedPoint;
 using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
 
 namespace Content.Shared._ES.Sparks.Components;
 
@@ -8,24 +7,12 @@ namespace Content.Shared._ES.Sparks.Components;
 /// An entity that sparks when damaged by something
 /// </summary>
 [RegisterComponent, NetworkedComponent]
-[Access(typeof(ESSparkOnHitSystem))]
-public sealed partial class ESSparkOnHitComponent : Component
+[Access(typeof(ESSparksSystem))]
+public sealed partial class ESSparkOnHitComponent : ESBaseSparkConfigurationComponent
 {
     /// <summary>
     /// Amount of damage that needs to be dealt to cause sparks
     /// </summary>
     [DataField]
     public FixedPoint2 Threshold = 1;
-
-    /// <summary>
-    /// Number of sparks
-    /// </summary>
-    [DataField]
-    public int Count = 3;
-
-    /// <summary>
-    /// Spark prototypes
-    /// </summary>
-    [DataField]
-    public EntProtoId SparkPrototype = ESSparksSystem.DefaultSparks;
 }
